a36a9b1d0e026895536799b7f2238eec756eba0d
Cameron Leslie <cameronleslie@gmail.com>
2017-07-08 10:08:27 -0700

    working

--- dist/bem-health-check.js
+++ dist/bem-health-check.js
@@ -1,6 +1,15 @@
 'use strict';
 
 (function (bemHealthCheck) {
+
+  var addOnClick = function addOnClick(id) {
+    return function (func) {
+      var el = document.getElementById(id);
+      console.log(el);
+      el.onclick = func;
+    };
+  };
+
   // console.log('loaded');
   var select = function select(type) {
     return function (selector) {
@@ -18,18 +27,46 @@
   var outline = function outline(selector) {
     return function (color) {
       return selector.forEach(function (item) {
-        return item.setAttribute("style", 'outline: 1px solid ' + color + ';');
+        if (!item.hasAttribute('style')) {
+          item.setAttribute('style', 'outline: 1px solid ' + color + ';');
+        } else {
+          item.removeAttribute('style');
+        }
       });
     };
   };
 
-  outline(objects)('purple');
-  outline(components)('blue');
-  outline(utilities)('orange');
-  outline(bemElement)('green');
-  outline(bemModifier)('yellow');
-
-  var createElement = function createElement(type) {
-    return document.createElement(type);
+  var outlineObjects = function outlineObjects() {
+    return outline(objects)('purple');
   };
+  var outlineComponents = function outlineComponents() {
+    return outline(components)('blue');
+  };
+  var outlineUtilities = function outlineUtilities() {
+    return outline(utilities)('orange');
+  };
+  var outlineElements = function outlineElements() {
+    return outline(bemElement)('green');
+  };
+  var outlineModifiers = function outlineModifiers() {
+    return outline(bemModifier)('yellow');
+  };
+
+  var addTools = function addTools() {
+    var tool = document.createElement("div");
+    tool.setAttribute('style', '\n      position: fixed;\n      bottom: 0;\n      left: 0;\n      right: 0;\n      background-color: black;\n      padding: .5rem;\n    ');
+    tool.innerHTML = '\n      <button id="objects">Objects</button>\n      <button id="components">Components</button>\n      <button id="utilities">Utilities</button>\n      <button id="elements">BEM Elements</button>\n      <button id="modifiers">BEM Modifiers</button>\n    ';
+
+    var context = document.body;
+    document.body.appendChild(tool);
+    //add onClick
+  };
+
+  addTools();
+
+  addOnClick('objects')(outlineObjects);
+  addOnClick('components')(outlineComponents);
+  addOnClick('utilities')(outlineUtilities);
+  addOnClick('elements')(outlineElements);
+  addOnClick('modifiers')(outlineModifiers);
 })(window);
--- src/bem-health-check.js
+++ src/bem-health-check.js
@@ -1,4 +1,11 @@
 (bemHealthCheck => {
+
+  const addOnClick = id => func => {
+    let el = document.getElementById(id);
+    console.log(el)
+    el.onclick = func;
+  };
+  
   // console.log('loaded');
   const select = type => selector => document.body.querySelectorAll(`[class${type}="${selector}"]`);
   const containsSelect = select('*');
@@ -9,15 +16,50 @@
   const bemElement = containsSelect('__');
   const bemModifier = containsSelect('--');
 
-  const outline = selector => color => selector.forEach(item => item.setAttribute("style", `outline: 1px solid ${color};`));
+  const outline = selector => color => selector.forEach(item => {
+    if (!item.hasAttribute('style')) {
+      item.setAttribute('style', `outline: 1px solid ${color};`);
+    } else {
+      item.removeAttribute('style')
+    }
+  });
 
-  outline(objects)('purple');
-  outline(components)('blue');
-  outline(utilities)('orange');
-  outline(bemElement)('green');
-  outline(bemModifier)('yellow');
+  const outlineObjects = () => outline(objects)('purple');
+  const outlineComponents = () => outline(components)('blue');
+  const outlineUtilities = () => outline(utilities)('orange');
+  const outlineElements = () => outline(bemElement)('green');
+  const outlineModifiers = () => outline(bemModifier)('yellow');
+  
+  const addTools = () => {
+    let tool = document.createElement("div");
+    tool.setAttribute('style',`
+      position: fixed;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      background-color: black;
+      padding: .5rem;
+    `);
+    tool.innerHTML = `
+      <button id="objects">Objects</button>
+      <button id="components">Components</button>
+      <button id="utilities">Utilities</button>
+      <button id="elements">BEM Elements</button>
+      <button id="modifiers">BEM Modifiers</button>
+    `;
 
-  const createElement = type => document.createElement(type);
+    let context = document.body;
+    document.body.appendChild(tool);
+    //add onClick
+  };
+  
+  addTools();
 
+  addOnClick('objects')(outlineObjects);
+  addOnClick('components')(outlineComponents);
+  addOnClick('utilities')(outlineUtilities);
+  addOnClick('elements')(outlineElements);
+  addOnClick('modifiers')(outlineModifiers);
 
+  
 })(window);
